43ec7c1236b0d1f8544229cd5ac68eadffa75704
antubhatta <antubhatta09@gmail.com>
2023-09-24 02:40:25 +0600

    add icons and add show all jobs

--- src/Components/FeatureJob/FeatureJob.jsx
+++ src/Components/FeatureJob/FeatureJob.jsx
@@ -1,7 +1,8 @@
 
-
+import { MdLocationOn} from 'react-icons/md';
+import { AiFillDollarCircle} from 'react-icons/ai';
 const FeatureJob = ({job}) => {
-   const {logo,job_title,company_name,remote_or_onsite,locarion,job_type,salary}=job
+   const {logo,job_title,company_name,remote_or_onsite,location,job_type,salary}=job
     return (
         <div className="bg-[#E8E8E8] rounded-lg border w-full border-solid pl-10 pt-10">
             <img className="mb-8" src={logo} alt="" />
@@ -11,6 +12,17 @@ const FeatureJob = ({job}) => {
                 <button className="text-base font-extrabold bg-gradient-to-r px-5 py-2 border rounded border-[#7E90FE] border-solid from-blue-400 to-purple-500 text-transparent bg-clip-text">{remote_or_onsite}</button>
                 <button className="text-base font-extrabold bg-gradient-to-r px-5 py-2 border rounded border-[#7E90FE] border-solid from-blue-400 to-purple-500 text-transparent bg-clip-text">{job_type}</button>
             </div>
+            <div className='flex gap-6 items-center mb-6'>
+                <div className='flex gap-1 items-center'>
+                    <h2><MdLocationOn className='text-2xl'></MdLocationOn></h2>
+                    <p className='text-base font-semibold text-[#757575]'>{location}</p>
+                </div>
+                <div className='flex gap-1 items-center'>
+                    <h2><AiFillDollarCircle className='text-2xl'></AiFillDollarCircle></h2>
+                    <p className='text-base font-semibold text-[#757575]'>{salary}</p>
+                </div>
+            </div>
+            <button className='border px-5 py-3 mb-12 rounded bg-gradient-to-r from-blue-400 to-purple-500 text-base text-[#FFF] font-extrabold'>View Details</button>
         </div>
     );
 };
--- src/Components/FeaturedJobs/Featurejobs.jsx
+++ src/Components/FeaturedJobs/Featurejobs.jsx
@@ -4,6 +4,9 @@ import FeatureJob from "../FeatureJob/FeatureJob";
 
 const Featuredjobs = () => {
     const [jobs, setJob]=useState([]);
+    const [dataLength,setDataLength]=useState(4)
+  
+    // this is not the best way to show all data
 
     useEffect(()=>{
         fetch('jobs.json')
@@ -14,8 +17,13 @@ const Featuredjobs = () => {
         <div>
             <h3 className="text-5xl mt-7 text-center font-extrabold">Featured Jobs:{jobs?.length}</h3>
             <p className="text-base text-center text-[#757575] mt-3 mb-8">Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            {jobs?.map(job=><FeatureJob key={job.id}job={job}></FeatureJob>)}
+            <div className="grid grid-cols-1 md:grid-cols-2 mb-10 gap-6">
+            {jobs?.slice(0,dataLength).map(job=><FeatureJob key={job.id}job={job}></FeatureJob>)}
+        </div>
+        <div className={dataLength===jobs.length ? 'hidden' :''}>
+        <div className="flex justify-center mb-28">
+            <button onClick={()=>setDataLength(jobs.length)} className="text-base text-[#FFF] font-extrabold px-7 py-4 bg-gradient-to-r from-blue-400 to-purple-500">See All Jobs</button>
+        </div>
         </div>
         </div>
        
